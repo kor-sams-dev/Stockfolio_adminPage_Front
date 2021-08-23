b4957382f5a07196d224a1c4f9f4870d55f34acc
luna <anachrosh@gmail.com>
2021-08-23 10:06:29 +0900

    feat(application): 레이아웃 및 기본 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import React from "react";
 
 import Nav from "./components/templates/Nav";
-import Landing from "./components/pages/Landing";
 import Footer from "./components/templates/Footer";
-import TeamIntro from "./components/pages/TeamIntro";
-import Application from "./components/templates/Application";
-
-// import Landing from "./components/pages/Landing";
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <Nav />
-      {/* <TeamIntro /> */}
-      {/* <Landing /> */}
-      <Application />
       <Footer />
     </div>
   );
