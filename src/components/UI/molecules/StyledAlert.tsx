839f1caab54febd921d970b8fe44c9957b36cbba
luna <anachrosh@gmail.com>
2021-08-26 20:17:32 +0900

    토큰 초기화

--- src/components/UI/molecules/StyledAlert.tsx
+++ src/components/UI/molecules/StyledAlert.tsx
@@ -45,6 +45,7 @@ const StyledAlert = (): JSX.Element => {
     setIsAlertOn();
     if (alertType === "applySuccess") {
       history.push("/recruit/process");
+      HandleToken.setUserToken({ access_token: "", is_applied: false });
     }
   };
 
